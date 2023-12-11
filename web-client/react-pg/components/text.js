10a2a4a464e672409b870bfd9f21b885903dab0d
Hung Bui <hngbv95@gmail.com>
2023-12-11 14:27:12 +0700

    useMemo: stop re render when prop is a ref value

--- web-client/react-pg/components/text.js
+++ web-client/react-pg/components/text.js
@@ -1,5 +1,5 @@
 import { useTextContext } from '../contexts/text'
-import { memo } from 'react'
+import { memo, useMemo } from 'react'
 
 function ChildComponent() {
     console.log('Child Component')
@@ -7,6 +7,15 @@ function ChildComponent() {
 }
 
 const ChildComponentMemo = memo(ChildComponent)
+
+const ChildComponentWithProp = memo(function ChildComponentWithProp({info}) {
+    console.log('(props)Child Component render')
+    const { name, age } = info
+    return <>
+        <h1>{name}</h1>
+        <h1>{age}</h1>
+    </>
+})
 
 export function Text() {
     const { setValue } = useTextContext()
@@ -20,11 +29,18 @@ export function Text() {
 
 export function TextDisplay() {
     const { value } = useTextContext()
+
+    const info = useMemo(() => {
+        return {
+            name: 'John Doe',
+            age: 24
+        }
+    }, [])
     return (
         <>
             <h1>{value}</h1>
             <ChildComponentMemo></ChildComponentMemo>
+            <ChildComponentWithProp info={info}></ChildComponentWithProp>
         </>
-        
     );
 }
